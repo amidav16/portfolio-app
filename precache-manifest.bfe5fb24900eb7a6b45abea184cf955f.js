6748850a21ec575b8fea12b27ecd02d76e4e75eb
amidav16 <davidamirth@gmail.com>
2020-06-19 19:19:48 +0200

    Updates

---
+++ precache-manifest.bfe5fb24900eb7a6b45abea184cf955f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "951a69142722e380f7fe5d6068b4502e",
+    "url": "/portfolio-app/index.html"
+  },
+  {
+    "revision": "91dfc74651b1e4350489",
+    "url": "/portfolio-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e86032bfd5d5ce9574ac",
+    "url": "/portfolio-app/static/js/2.7fdd50ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-app/static/js/2.7fdd50ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91dfc74651b1e4350489",
+    "url": "/portfolio-app/static/js/main.77f7ebe3.chunk.js"
+  },
+  {
+    "revision": "9273fb07e8f782d2211c",
+    "url": "/portfolio-app/static/js/runtime-main.9d41a791.js"
+  }
+]);
